cc4577bb2a80dd5b6ab88a16179b5384c9e81415
Rsj1817 <20243ds185@utez.edu.mx>
2025-11-07 07:55:49 -0600

    Commit con apunte

---
+++ Desktop/EstructuraB/TDAEXAMPLE/src/ApuntesDeBubbleSort/RunBubbleSort.java
@@ -0,0 +1,10 @@
+package ApuntesDeBubbleSort;
+
+public class RunBubbleSort {
+    public static void main(String[] args) throws Exception {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] arr = {1,5,9,10,3};
+        bubbleSort.sort(arr);
+
+    }
+}
