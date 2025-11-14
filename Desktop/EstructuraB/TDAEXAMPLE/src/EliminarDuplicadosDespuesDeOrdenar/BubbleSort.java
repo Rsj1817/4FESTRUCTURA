6a06aa22f876507aee1cc2a265bce9eac0469f5b
Rsj1817 <20243ds185@utez.edu.mx>
2025-11-14 08:57:26 -0600

    Commit con ejercicio

--- Desktop/EstructuraB/TDAEXAMPLE/src/EliminarDuplicadosDespuesDeOrdenar/BubbleSort.java
+++ Desktop/EstructuraB/TDAEXAMPLE/src/EliminarDuplicadosDespuesDeOrdenar/BubbleSort.java
@@ -1,4 +1,58 @@
 package EliminarDuplicadosDespuesDeOrdenar;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+
+    public void sort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public int[] eliminarDuplicados(int[] arr) {
+        if (arr.length == 0) {
+            return new int[0];
+        }
+
+        int contadorUnicos = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                contadorUnicos++;
+            }
+        }
+
+        int[] sinDuplicados = new int[contadorUnicos];
+        sinDuplicados[0] = arr[0];
+        int indiceNuevo = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                sinDuplicados[indiceNuevo] = arr[i];
+                indiceNuevo++;
+            }
+        }
+
+        return sinDuplicados;
+    }
+
+    public void imprimirArreglo(String mensaje, int[] arr) {
+        System.out.println(mensaje + " " + Arrays.toString(arr));
+    }
 }
--- Desktop/EstructuraB/TDAEXAMPLE/src/EliminarDuplicadosDespuesDeOrdenar/RunBS.java
+++ Desktop/EstructuraB/TDAEXAMPLE/src/EliminarDuplicadosDespuesDeOrdenar/RunBS.java
@@ -1,4 +1,20 @@
 package EliminarDuplicadosDespuesDeOrdenar;
 
 public class RunBS {
+    public static void main(String[] args) {
+
+        int[] numeros = {4, 2, 9, 2, 4, 7, 1};
+
+        BubbleSort bs = new BubbleSort();
+
+        bs.imprimirArreglo("Arreglo original:", numeros);
+        bs.sort(numeros);
+
+        bs.imprimirArreglo("Arreglo ordenado:", numeros);
+
+        int[] sinDuplicados = bs.eliminarDuplicados(numeros);
+
+        bs.imprimirArreglo("Arreglo ordenado (con duplicados):", numeros);
+        bs.imprimirArreglo("Arreglo sin duplicados:", sinDuplicados);
+    }
 }
